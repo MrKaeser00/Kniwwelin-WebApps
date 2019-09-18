21c346e591fb6677b978cbb5b995b257b75175d6
BuildTools <unconfigured@null.spigotmc.org>
2019-09-18 23:48:56 +0200

    initial upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Kniwwelino-Web #
+
+This project is an expansion of the Kniwwelino project by [Luxembourg Institute of Science and Technology](http://www.list.lu). 
+A link to the [Kniwwelino&reg;](https://github.com/LIST-LUXEMBOURG/KniwwelinoLib) GitHub project. 
+At the moment this text is just a basic place-holder.
+
+## License
+This expansion is under the Lesser General Public License (LGPL) as is [Kniwwelino&reg;](http://kniwwelino.lu/en/).
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <Kniwwelino.h>
+
+void setup()
+{
+  //Initialize the Kniwwelino Board
+  Kniwwelino.begin("sketch_sep16a", false, true, false); // Wifi=true, Fastboot=true, MQTT Logging=false
+}
+
+void loop()
+{
+  char conn = (Kniwwelino.isConnected() == true ? '1' : '0');
+  char str[2];
+  str[0] = conn;
+  str[1] = '\0';
+  if (Kniwwelino.isConnected())
+  {
+    Kniwwelino.MATRIXwrite(Kniwwelino.getIP(), 1, false);
+  }
+
+  Kniwwelino.RGBsetBrightness((int)250);
+  Kniwwelino.MATRIXwriteOnce(str);
+
+  if (Kniwwelino.BUTTONAclicked())
+  {
+    for (int count = 0; count < 10; count++)
+    {
+      Kniwwelino.RGBsetColorEffect(String("A1FF46"), RGB_ON, -1);
+      Kniwwelino.sleep((unsigned long)(5 * 1000));
+      Kniwwelino.RGBsetColorEffect(String("FF0000"), RGB_BLINK, -1);
+      Kniwwelino.sleep((unsigned long)(5 * 1000));
+      Kniwwelino.loop(); // do background stuff...
+    }
+  }
+  if (Kniwwelino.BUTTONBclicked())
+  {
+    for (int count2 = 0; count2 < 10; count2++)
+    {
+      Kniwwelino.RGBsetColorEffect(String("3366FF"), RGB_FLASH, -1);
+      Kniwwelino.sleep((unsigned long)(5 * 1000));
+      Kniwwelino.RGBsetColorEffect(String("CC33CC"), RGB_GLOW, -1);
+      Kniwwelino.sleep((unsigned long)(5 * 1000));
+      Kniwwelino.loop(); // do background stuff...
+    }
+  }
+  if (Kniwwelino.BUTTONABclicked())
+  {
+    Kniwwelino.RGBclear();
+  }
+
+  Kniwwelino.loop(); // do background stuff...
+}
